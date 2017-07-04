8d944fc47cd57d90ddb81c913b7893d738b1a8c1
albedoboot <myjobemail1@163.com>
2017-07-04 16:16:51 +0800

    剔除无关配置

--- albedo-boot-data/albedo-boot-data-mybatis/albedo-boot-data-mybatis-repository/src/main/java/com/albedo/java/common/config/DatabaseAutoConfiguration.java
+++ albedo-boot-data/albedo-boot-data-mybatis/albedo-boot-data-mybatis-repository/src/main/java/com/albedo/java/common/config/DatabaseAutoConfiguration.java
@@ -19,10 +19,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 import java.util.Date;
 
-@EntityScan(basePackages = {"com.albedo.java.modules.*.domain", "com.qingju.java.modules.*.domain"})
+@EntityScan(basePackages = {"com.albedo.java.modules.*.domain"})
 @Configuration
 @EnableMybatisRepositories(
-        value = {"com.albedo.java.modules.*.repository", "com.qingju.java.modules.*.repository"},
+        value = {"com.albedo.java.modules.*.repository"},
         mapperLocations = "classpath*:/mappings/modules/*/*Mapper.xml"
 )
 @EnableTransactionManagement
