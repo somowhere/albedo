fcfbf73264254096586ee31b8b2834e66b8cf79f
somewhere <837158334@qq.com>
2020-06-13 16:28:51 +0800

    update ui build config

--- albedo-ui/src/router/routers.js
+++ albedo-ui/src/router/routers.js
@@ -62,7 +62,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
