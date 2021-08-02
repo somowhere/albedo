773a1d1d60cbe9470ee4b2736d024f53291052c3
slanlee <lee.satlan@stengg.com>
2021-08-02 15:09:53 +0800

    Regular upgrade[3.1.0-SNAPSHOT]

--- albedo-common/albedo-common-api/src/main/java/com/albedo/java/common/config/LogAutoConfiguration.java
+++ albedo-common/albedo-common-api/src/main/java/com/albedo/java/common/config/LogAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019-2020, somewhere (wangiegie@gmail.com).
+ *  Copyright (c) 2019-2021, somewhere (wangiegie@gmail.com).
  *  <p>
  *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
  *  you may not use this file except in compliance with the License.
